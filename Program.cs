5598aec74b24a14b91101cf08244e672d0ff9598
zhouchengjie <zhouchengjie@github.com>
2020-12-08 18:23:14 +0800

    excel export

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] updateData = { new string[] { "1", "CGTN", "频道xxx","纪实", "197", "5.0", "8.2%" }, new string[] { "2", "CGTN", "频道", "纪实", "197", "5.0", "0.2%" } };
+            string path = Environment.CurrentDirectory + "\\Template.xlsx";
+            NPOIHelper.UpdateExcel2(path, "sheet1", updateData, 1);
+        }
+    }
+}
